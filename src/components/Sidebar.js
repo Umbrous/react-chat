123228826263f8659e84c5e932e7bc816b962150
Umbrous <kozurevav@gmail.com>
2018-03-01 11:54:39 +0200

    add routing and auth form

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { withStyles } from 'material-ui/styles'
+import { withStyles } from 'material-ui/styles';
 import Drawer from 'material-ui/Drawer';
 import Divider from 'material-ui/Divider';
-import List, { ListItem, ListItemText } from 'material-ui/List';
-import Button from 'material-ui/Button';
-import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavigation';
 import TextField from 'material-ui/TextField';
-import Avatar from 'material-ui/Avatar';
-
-import titleInitials from '../utils/title-initials'
-
+import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavigation';
+import ChatList from './ChatList';
 import RestoreIcon from 'material-ui-icons/Restore';
 import ExploreIcon from 'material-ui-icons/Explore';
-import AddIcon from 'material-ui-icons/Add';
 
 const styles = theme => ({
-  chatsList: {
-    height: 'calc(100% - 56px)',
-    overflowY: 'scroll',
-  },
   drawerPaper: {
     position: 'relative',
     height: '100%',
@@ -28,17 +18,10 @@ const styles = theme => ({
     ...theme.mixins.toolbar,
     paddingLeft: theme.spacing.unit * 3,
     paddingRight: theme.spacing.unit * 3,
-  },
-  newChatButton: {
-    position: 'absolute',
-    left: 'auto',
-    right: theme.spacing.unit * 3,
-    bottom: theme.spacing.unit * 3 + 48,
   }
 });
 
 const Sidebar = ({ classes, chats }) => (
-
   <Drawer
     variant="permanent"
     classes={{
@@ -53,25 +36,12 @@ const Sidebar = ({ classes, chats }) => (
       />
     </div>
     <Divider />
-    <List className={classes.chatsList}>
-      {chats.map((chat, index) => (
-        <ListItem key={index} button>
-          <Avatar className={classes.avatar}>
-            {titleInitials(chat.title)}
-          </Avatar>
-          <ListItemText primary={chat.title} />
-        </ListItem>
-      ))}
-    </List>
-    <Button variant="fab" color="primary" className={classes.newChatButton}>
-      <AddIcon />
-    </Button>
+    <ChatList chats={chats} />
     <BottomNavigation showLabels>
-      <BottomNavigationAction label="My Chat" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="My Chats" icon={<RestoreIcon />} />
       <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
     </BottomNavigation>
   </Drawer>
-
 );
 
 export default withStyles(styles)(Sidebar);
